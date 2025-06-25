eb96fb2fce62c909e770567b362f023b4069a352
Nicolas Fiorini <hnfiorini@gmail.com>
2025-06-25 23:29:59 +0200

    Initial commit

---
+++ src/main/java/com/ravenpack/ufa/UserFlagApplication.java
@@ -0,0 +1,13 @@
+package com.ravenpack.ufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFlagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFlagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravenpack/ufa/UserFlagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravenpack.ufa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFlagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
